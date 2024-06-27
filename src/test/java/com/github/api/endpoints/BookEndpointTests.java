d3498ebb420a7054db32dc427b53391a16f161fd
mattand <mjande@umich.edu>
2024-06-27 12:04:16 -0400

    Add test for getAllBooks and configure testing setup

---
+++ src/test/java/com/github/api/endpoints/BookEndpointTests.java
@@ -0,0 +1,29 @@
+package com.github.api.endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class BookEndpointTests {
+    
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void getAllBooksReturnsString() {
+        webTestClient.get()
+            .uri("/books")
+            .exchange()
+            .expectBody(String.class)
+            .isEqualTo("All books");
+    }
+}
