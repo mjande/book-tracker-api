027a5aae8cabc8601be8a45d1d97a7eb2f3c2930
mattand <mjande@umich.edu>
2024-06-27 10:51:00 -0400

    Create book model class

---
+++ src/main/java/com/github/api/models/Book.java
@@ -0,0 +1,40 @@
+package com.github.api.models;
+
+public class Book {
+    
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String genre;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
