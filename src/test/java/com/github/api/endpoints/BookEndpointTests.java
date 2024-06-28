f64bf50428f384fb3d174708253b14ef6564ab7f
mattand <mjande@umich.edu>
2024-06-28 15:23:55 -0400

    Finish basic endpoints tests for Books

--- src/test/java/com/github/api/endpoints/BookEndpointTests.java
+++ src/test/java/com/github/api/endpoints/BookEndpointTests.java
@@ -8,6 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.github.api.models.Book;
+
+import reactor.core.publisher.Mono;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class BookEndpointTests {
@@ -34,5 +38,52 @@ public class BookEndpointTests {
             .exchange()
             .expectBody(String.class)
             .isEqualTo("Book with ID 2");
+    }
+
+    @Test
+    public void createBookReturnsString() {
+        Book newBook = new Book();
+        newBook.setId(1);
+        newBook.setTitle("TestBook");
+        newBook.setAuthor("TestAuthor");
+        newBook.setPublicationYear(2024);
+        newBook.setGenre("TestGenre");
+
+        Mono<Book> bookMono = Mono.just(newBook);
+        
+        webTestClient.post()
+            .uri("/books")
+            .body(bookMono, Book.class)
+            .exchange()
+            .expectBody(String.class)
+            .isEqualTo("Book was created: " + newBook.toString());
+    }
+
+    @Test
+    public void updateBookReturnsString() {
+        Book updatedBook = new Book();
+        updatedBook.setId(1);
+        updatedBook.setTitle("TestBook");
+        updatedBook.setAuthor("TestAuthor");
+        updatedBook.setPublicationYear(2024);
+        updatedBook.setGenre("TestGenre");
+
+        Mono<Book> bookMono = Mono.just(updatedBook);
+
+        webTestClient.put()
+            .uri("/books/1")
+            .body(bookMono, Book.class)
+            .exchange()
+            .expectBody(String.class)
+            .isEqualTo("Book with id 1 was updated: " + updatedBook.toString());
+    }
+
+    @Test
+    public void deleteBookReturnsString() {
+        webTestClient.delete()
+            .uri("/books/2")
+            .exchange()
+            .expectBody(String.class)
+            .isEqualTo("Book with id 2 was deleted.");
     }
 }
